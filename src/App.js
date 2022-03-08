be64b5846039142c73570e0de22abb78c4da674c
Pedro Barão <phbarao@gmail.com>
2022-03-07 23:25:23 -0300

    initial comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import SearchInput from './components/SearchInput';
+
+const api = 'https://kitsu.io/api/edge/';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    if (text.length >= 3) {
+      console.log(text);
+      fetch(`${api}anime?filter[text]=${text}&page[limit]=12`)
+        .then((response) => response.json())
+        .then((response) => {
+          console.log(response);
+        });
+    }
+  }, [text]);
+
+  return <SearchInput value={text} onChange={(search) => setText(search)} />;
 }
 
 export default App;
